46f9f91518a480ec6049c2166527168bb1b2a006
EXTREME <denisfirstwp@gmail.com>
2020-08-14 08:18:12 -0300

    quarto commit

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 
-
-app.get('/',(req,res)=>res.send('Welcome to the first API'))
+app.get('/', (req,res)=>{
+  res.json({
+    msg:'OK'
+  })
+})
 
 app.listen(8877);
